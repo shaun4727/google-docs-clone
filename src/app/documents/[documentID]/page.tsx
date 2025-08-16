7d0ae23363de32b5b2a2b72b8d5d4165ddce24aa
Shaun Hossain <107396795+shaun4727@users.noreply.github.com>
2025-08-16 19:10:45 +0600

    add tiptap editors functionality

---
+++ src/app/documents/[documentID]/page.tsx
@@ -0,0 +1,12 @@
+import EditorPage from './editor';
+
+const DocumentIDPage = () => {
+	return (
+		<div>
+			<h1>Document ID Page</h1>
+			<EditorPage />
+		</div>
+	);
+};
+
+export default DocumentIDPage;
